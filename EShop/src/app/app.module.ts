d6de0019c26e824ee4dd5ff09f197217b0051e5c
tanishraj <tanishraj.quitegrowth@gmail.com>
2018-09-29 23:05:06 +0530

    players component has been removed as there is some issue with aiuxiliary routing. For Players Routing Assignment A new project is generated and and you can find this inside the same infocampus training folder.

--- EShop/src/app/app.module.ts
+++ EShop/src/app/app.module.ts
@@ -39,7 +39,6 @@ import { CompanyDynamicFormComponent } from './company-dynamic-form/company-dyna
 import { PePipesComponent } from './pe-pipes/pe-pipes.component';
 import { ArrayPipeComponent } from './array-pipe/array-pipe.component';
 import { ObservablePostsComponent } from './observable-posts/observable-posts.component';
-import { RoutingPlayersComponent } from './routing-players/routing-players.component';
 import { SachinComponent } from './sachin/sachin.component';
 import { DravidComponent } from './dravid/dravid.component';
 import { AzaharuddinComponent } from './azaharuddin/azaharuddin.component';
@@ -75,7 +74,6 @@ import { AzaharuddinComponent } from './azaharuddin/azaharuddin.component';
 		ArrayPipeComponent,
 		ArrayLimit,
 		ObservablePostsComponent,
-		RoutingPlayersComponent,
 		SachinComponent,
 		DravidComponent,
 		AzaharuddinComponent
@@ -91,11 +89,6 @@ import { AzaharuddinComponent } from './azaharuddin/azaharuddin.component';
 			{path: 'Contact', component: ContactComponent},
 			{path: 'Login/:val', component: LoginComponent},
 			{path: 'Welcome', component: WelcomeComponent},
-
-			//players routing
-			{path: 'Sachin', component: SachinComponent},
-			{path: 'Dravid', component: DravidComponent},
-			{path: 'Azaharuddin', component: AzaharuddinComponent}
 		])
 	],
 	providers: [
--- EShop/src/app/routing/routing.component.ts
+++ EShop/src/app/routing/routing.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RoutingComponent implements OnInit {
 
-  constructor() { }
+  constructor(private x: ActivatedRoute) { }
 
   ngOnInit() {
+    this.x.params.subscribe(
+      (res) => {
+        console.log("The parameter is =" + res['val']);
+      }
+    )
   }
 
 }
