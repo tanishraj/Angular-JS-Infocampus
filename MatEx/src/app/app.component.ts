da5cad8fdc5f90d2c9ab83114b636ea210d8f68c
tanishraj <tanishraj.quitegrowth@gmail.com>
2018-10-14 15:10:01 +0530

    Angular Material has been implemented.

--- MatEx/src/app/app.component.ts
+++ MatEx/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'MatEx';
+  
+  checked = false;
+  indeterminate = false;
+  labelPosition = 'after';
+  disabled = false;
 }
